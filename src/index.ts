95016afe601764a662cab06e9f8805d3c76b319e
Adam Rasfeld <arasfeld@gmail.com>
2022-10-15 05:07:26 -0400

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
+import 'dotenv/config'
 import express from 'express'
 
 const app = express()
-const port = 3000
+const port = parseInt(process.env.PORT || '', 10) || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.listen(port, () => {
-  console.log(`🚀 Server ready at http://localhost:${port}`);
+  console.log(`🚀 Server ready at http://localhost:${port}`)
 })
