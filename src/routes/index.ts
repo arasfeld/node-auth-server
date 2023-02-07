a459e68332804bced1c590a7783786698cb03767
Adam Rasfeld <arasfeld@gmail.com>
2023-02-07 14:42:20 -0500

    add /me route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { loginRouter } from './login-routes'
 import { registrationRouter } from './registration-routes'
+import { userRouter } from './user-routes'
 
 const router = Router()
 
 router.use(loginRouter)
 router.use(registrationRouter)
+router.use(userRouter)
 
 export default router
