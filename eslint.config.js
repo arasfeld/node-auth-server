136e07971fc300e30a40926d5201488e28b071a7
Adam Rasfeld <arasfeld@gmail.com>
2025-11-22 08:42:38 -0500

    chore: update ESLint to 9.x and migrate to flat config
    - Upgrade eslint from 8.32.0 to 9.39.1 (major version)
    - Upgrade @typescript-eslint/eslint-plugin from 5.48.2 to 8.47.0
    - Upgrade @typescript-eslint/parser from 5.48.2 to 8.47.0
    - Add @eslint/js for flat config support
    - Migrate from .eslintrc.js to eslint.config.js (flat config format)
    - Add Node.js globals to ESLint config
    - Remove deprecated .eslintrc.js file

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+const js = require('@eslint/js');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+const path = require('path');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        global: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...tseslint.configs['recommended-requiring-type-checking'].rules,
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+];
+
